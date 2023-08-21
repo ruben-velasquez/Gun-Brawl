9592f0d99cf9088bc261e26ef0b24079710d4da5
ruben-velasquez <rualvemo@hotmail.com>
2023-08-20 22:41:19 -0300

    Arreglado Bug de movimiento en el menú principal
    Este bug provocaba que los contenedores que separan el contenido de cada ventana (Principal, Modos de juego, Jugadores, Configuración) no estuvieran centrados en la pantalla

--- Assets/Scripts/UI/Elements/ContainersGroup.cs
+++ Assets/Scripts/UI/Elements/ContainersGroup.cs
@@ -36,14 +36,14 @@ namespace UI
 
             page = newPage;
 
-            float posX = 134 - (268 * page);
+            float posX = -(268 * page);
 
             LeanTween.alphaCanvas(containers[page], 1, 1f).setDelay(1.5f);
 
             containers[page].interactable = true;
             containers[page].blocksRaycasts = true;
 
-            LeanTween.move(rectTransform, new Vector3(posX, -84f, 0f), 0.5f).setEase(transition).setDelay(1f);
+            LeanTween.move(rectTransform, new Vector3(posX, 0f, 0f), 0.5f).setEase(transition).setDelay(1f);
         }
     }
 }
