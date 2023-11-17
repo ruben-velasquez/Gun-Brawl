ccba570627c51af13bdd0b41dffef68d205280c4
ruben-velasquez <rualvemo@hotmail.com>
2023-11-16 21:43:39 -0300

    Animate game mode information

--- Assets/Scripts/UI/Elements/GameModeInformation.cs
+++ Assets/Scripts/UI/Elements/GameModeInformation.cs
@@ -1,5 +1,7 @@
 using UnityEngine.UI;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class GameModeInformation : MonoBehaviour
 {
@@ -9,6 +11,12 @@ public class GameModeInformation : MonoBehaviour
     private Text description;
     [SerializeField]
     private Image image;
+    private CanvasGroup canvasGroup;
+
+
+    private void Start() {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
 
     public void SetTitle(string content) {
         title.text= content;
@@ -21,8 +29,19 @@ public class GameModeInformation : MonoBehaviour
     }
 
     public void Set(GameMode.GameMode gameMode) {
+        if(gameMode.name == title.text) return;
+        StartCoroutine(SetInfoAnim(gameMode));
+    }
+
+    private IEnumerator SetInfoAnim(GameMode.GameMode gameMode) {
+        LeanTween.alphaCanvas(canvasGroup, 0, 0.2f);
+
+        yield return new WaitForSeconds(0.2f);
+
         SetTitle(gameMode.name);
         SetDescription(gameMode.description);
         SetImage(gameMode.sprite);
+
+        LeanTween.alphaCanvas(canvasGroup, 1, 0.3f);
     }
 }
