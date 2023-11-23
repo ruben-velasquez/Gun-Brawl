198444e0f86fd5019d91b7ebc2033547822d2590
ruben-velasquez <rualvemo@hotmail.com>
2023-11-23 20:47:02 -0300

    Improve manual friction of obstacles
    Now the item boxes and the barrels have a better friction system.
    
    This new system check if the object is in the floor and if it's touching something.

--- Assets/Scripts/Physics/ManualFriction.cs
+++ Assets/Scripts/Physics/ManualFriction.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 public class ManualFriction : MonoBehaviour
 {
     [SerializeField]
+    private Vector2 globalRaySideOffset = new Vector2(0.25f, 0f);
+    [SerializeField]
+    private float rayLength = 0.85f;
+    [SerializeField]
+    private Vector2 boxSize = new Vector2(1f, 1f);
+    [SerializeField]
+    private LayerMask groudMask;
+    [SerializeField]
     private float frictionForce = 10f;
     private Rigidbody2D rb;
     private bool isOnGround;
+    private bool isOnGroundAbove;
 
     private void Start()
     {
@@ -14,10 +23,45 @@ public class ManualFriction : MonoBehaviour
 
     private void Update()
     {
-        if (isOnGround)
+        CheckGroundAbove();
+
+        if (isOnGround && isOnGroundAbove)
         {
             rb.AddForce(-rb.velocity * frictionForce);
         }
+    }
+
+    private void CheckGroundAbove()
+    {
+        Vector2 positionCenter = (Vector2)transform.position + new Vector2(0, globalRaySideOffset.y);
+        Vector2 direction = Vector2.down;
+
+        RaycastHit2D[] hits = Physics2D.BoxCastAll(positionCenter, boxSize, 0f, direction, rayLength, groudMask);
+
+        // Dibujar el BoxCast
+        Debug.DrawRay(positionCenter, direction * rayLength, Color.red);
+
+        // Dibujar el cubo
+        Vector2 topLeft = positionCenter + new Vector2(-boxSize.x, boxSize.y) / 2;
+        Vector2 topRight = positionCenter + new Vector2(boxSize.x, boxSize.y) / 2;
+        Vector2 bottomLeft = positionCenter + new Vector2(-boxSize.x, -boxSize.y) / 2;
+        Vector2 bottomRight = positionCenter + new Vector2(boxSize.x, -boxSize.y) / 2;
+
+        Debug.DrawRay(topLeft, Vector2.right * boxSize.x, Color.red);
+        Debug.DrawRay(topLeft, Vector2.down * boxSize.y, Color.red);
+        Debug.DrawRay(bottomRight, Vector2.left * boxSize.x, Color.red);
+        Debug.DrawRay(bottomRight, Vector2.up * boxSize.y, Color.red);
+
+        foreach (var hit in hits)
+        {
+            if (hit.collider != null && hit.collider.gameObject != this.gameObject)
+            {
+                isOnGroundAbove = true;
+                return;
+            }
+        }
+
+        isOnGroundAbove = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
